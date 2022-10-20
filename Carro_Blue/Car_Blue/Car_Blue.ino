2b04ba9dc3335bb93c4f3f079e0ff78f164a4288
wendellast <wendelalves24@hotmail.com>
2022-10-20 20:32:13 -0300

    Atualizando carro, melhoria na musica e função de balançar a cabeça

--- Carro_Blue/Car_Blue/Car_Blue.ino
+++ Carro_Blue/Car_Blue/Car_Blue.ino
@@ -1,24 +1,30 @@
 #include <SoftwareSerial.h>
 #include "music.h"
 #include "music_jojo.h"
+#include <Servo.h>
 
 SoftwareSerial bluetooth(8, 7);
 
-char comando;
+char comando; // Comando BLUE
 
+//Servo Motor
+#define pinServo1 11
+Servo servoCabeca;
+//Servo Motor
+
+//Motores
 int IN1 = 4;
 int IN2 = 5;
 
 int IN3 = 6;
 int IN4 = 1;
+//Motores
 
-int xz = 0;
+int xz = 0;  // Musica sorted
 
-int led1 = 9;  // Led1 -
+int led1_azul = 9;  // Led1 - Azul Alerta
 
 int buzzerPin = 10;  // Buzina
-int speakPin = 10; // Buzina
-
 
 
 
@@ -32,11 +38,17 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  pinMode(led1, OUTPUT);
+  pinMode(led1_azul, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
-  digitalWrite(led1, LOW);
+  servoCabeca.attach(pinServo1);  
+  
+  iniciar();
+  cabeca_reta();
+  naoCabeca();
+  cabeca_reta();
 }
+
 
 
 void loop() {
@@ -71,11 +83,11 @@ void loop() {
     }
 
     else if (comando == 'X') {
-      digitalWrite(led1, HIGH);
+      digitalWrite(led1_azul, HIGH);
     }
 
     else if (comando == 'x') {
-      digitalWrite(led1, LOW);
+      digitalWrite(led1_azul, LOW);
     }
 
     else {
@@ -123,6 +135,47 @@ void parado() {
   digitalWrite(IN4, LOW);
 }
 
+
+void iniciar() {
+
+  //Piscar LED AZUL
+
+
+  digitalWrite(led1_azul, HIGH);
+  delay(500);
+  digitalWrite(led1_azul, LOW);
+  delay(500);
+  digitalWrite(led1_azul, HIGH);
+  delay(500);
+  digitalWrite(led1_azul, LOW);
+  delay(500);
+  digitalWrite(led1_azul, HIGH);
+  delay(500);
+  digitalWrite(led1_azul, LOW);
+}
+
+//FUNÇÃO PARA BALANCAR CABEÇA
+void naoCabeca(){
+  
+
+  servoCabeca.write(40);
+  delay(250);  
+  servoCabeca.write(140);
+  delay(250); 
+  servoCabeca.write(40);
+  delay(250);
+  //servoCabeca.write(140);
+  //delay(250);
+  servoCabeca.write(90);
+  delay(250);
+}
+
+void cabeca_reta(){
+  servoCabeca.write(90);
+  delay(100);
+  servoCabeca.write(10);
+  delay(100);
+}
 void beep(int speakerPin, float noteFrequency, long noteDuration) {
   int x;
 
@@ -139,112 +192,67 @@ void beep(int speakerPin, float noteFrequency, long noteDuration) {
 
 void buzina() {
 
-    xz = random(1, 5); // Vai de 0 a 4
-    
-    if (xz == 2)    { // Musica robot
-    beep(buzzerPin, note_A7, 100);  //A
-    beep(buzzerPin, note_G7, 100);  //G
-    beep(buzzerPin, note_E7, 100);  //E
-    beep(buzzerPin, note_C7, 100);  //C
-    beep(buzzerPin, note_D7, 100);  //D
-    beep(buzzerPin, note_B7, 100);  //B
-    beep(buzzerPin, note_F7, 100);  //F
-    beep(buzzerPin, note_C8, 100);  //C
-    beep(buzzerPin, note_A7, 100);  //A
-    beep(buzzerPin, note_G7, 100);  //G
-    beep(buzzerPin, note_E7, 100);  //E
-    beep(buzzerPin, note_C7, 100);  //C
-    beep(buzzerPin, note_D7, 100);  //D
-    beep(buzzerPin, note_B7, 100);  //B
-    beep(buzzerPin, note_F7, 100);  //F
-    beep(buzzerPin, note_C8, 100);  //C
-    }
+  xz = random(2, 4);  // Vai de 0 a 4
 
-    else if (xz == 3){ // Musica JOJO
-    beep(buzzerPin, f5s, Qnote+Enote);
-    beep(buzzerPin,  d5, Hnote);
-    beep(buzzerPin,  d5, Snote);
-    beep(buzzerPin,  e5, Snote);
-    beep(buzzerPin,  f5, Enote+Snote);
-    beep(buzzerPin,  e5, Enote+Snote);
-    beep(buzzerPin,  d5, Enote);
-    beep(buzzerPin,  c5s, Enote+Snote);
-    beep(buzzerPin,  d5, Enote+Snote);
-    beep(buzzerPin,  e5, Enote);
-    beep(buzzerPin, f5s, Qnote+Enote);
-    beep(buzzerPin, b5, Qnote+Enote);
+  if (xz == 2) {                    // Musica robot
+    beep(buzzerPin, note_A7, 100);  //A
+    beep(buzzerPin, note_G7, 100);  //G
+    beep(buzzerPin, note_E7, 100);  //E
+    beep(buzzerPin, note_C7, 100);  //C
+    beep(buzzerPin, note_D7, 100);  //D
+    beep(buzzerPin, note_B7, 100);  //B
+    beep(buzzerPin, note_F7, 100);  //F
+    beep(buzzerPin, note_C8, 100);  //C
+    beep(buzzerPin, note_A7, 100);  //A
+    beep(buzzerPin, note_G7, 100);  //G
+    beep(buzzerPin, note_E7, 100);  //E
+    beep(buzzerPin, note_C7, 100);  //C
+    beep(buzzerPin, note_D7, 100);  //D
+    beep(buzzerPin, note_B7, 100);  //B
+    beep(buzzerPin, note_F7, 100);  //F
+    beep(buzzerPin, note_C8, 100);  //C
+  }
+
+  else if (xz == 3) {  // Musica JOJO
+    beep(buzzerPin, f5s, Qnote + Enote);
+    beep(buzzerPin, d5, Hnote);
+    beep(buzzerPin, d5, Snote);
+    beep(buzzerPin, e5, Snote);
+    beep(buzzerPin, f5, Enote + Snote);
+    beep(buzzerPin, e5, Enote + Snote);
+    beep(buzzerPin, d5, Enote);
+    beep(buzzerPin, c5s, Enote + Snote);
+    beep(buzzerPin, d5, Enote + Snote);
+    beep(buzzerPin, e5, Enote);
+    beep(buzzerPin, f5s, Qnote + Enote);
+    beep(buzzerPin, b5, Qnote + Enote);
     beep(buzzerPin, b4, Enote);
     beep(buzzerPin, c5, Enote);
-    beep(buzzerPin,  d5, Enote+Snote);
-    beep(buzzerPin,  e5, Enote+Snote);
-    beep(buzzerPin,  d5, Enote);
-    beep(buzzerPin,  c5s, Enote+Snote);
-    beep(buzzerPin,  a5, Enote+Snote);
-    beep(buzzerPin,  g5, Enote);
-    beep(buzzerPin,  f6, Qnote+Enote);
-    beep(buzzerPin,  d6, Hnote);
-    beep(buzzerPin,  d6, Snote);
-    beep(buzzerPin,  e6, Snote);
-    beep(buzzerPin,  f6, Enote+Snote);
-    beep(buzzerPin,  e6, Enote+Snote);
-    beep(buzzerPin,  d6, Enote;
-    beep(buzzerPin,  c6, Enote+Snote);
-    beep(buzzerPin,  d6, Enote+Snote);
-    beep(buzzerPin,  e6, Enote);
-   beep(buzzerPin,  f6, Qnote+Enote);
-     beep(buzzerPin,  b6, Qnote+Enote);
-
-  
-    tone(speakPin, b6);
-    delay(Enote);
-    noTone(speakPin);
-    delay(50);
-
-    tone(speakPin, c7s);
-    delay(Enote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, d7);
-    delay(Enote+Snote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, g6);
-    delay(Enote+Snote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, f6s);
-    delay(Enote);
-    noTone(speakPin);
-    delay(50);
-
-    tone(speakPin, f6);
-    delay(Snote+Enote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, d7);
-    delay(Enote+Snote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, a6s);
-    delay(Enote);
-    noTone(speakPin);
-    delay(25);
-
-    tone(speakPin, b6);
-    delay(Qnote);
-    noTone(speakPin);
-    delay(25);      
-    }
-
-
-
-
-
+    beep(buzzerPin, d5, Enote + Snote);
+    beep(buzzerPin, e5, Enote + Snote);
+    beep(buzzerPin, d5, Enote);
+    beep(buzzerPin, c5s, Enote + Snote);
+    beep(buzzerPin, a5, Enote + Snote);
+    beep(buzzerPin, g5, Enote);
+    beep(buzzerPin, f6, Qnote + Enote);
+    beep(buzzerPin, d6, Hnote);
+    beep(buzzerPin, d6, Snote);
+    beep(buzzerPin, e6, Snote);
+    beep(buzzerPin, f6, Enote + Snote);
+    beep(buzzerPin, e6, Enote + Snote);
+    beep(buzzerPin, d6, Enote);
+    beep(buzzerPin, c6, Enote + Snote);
+    beep(buzzerPin, d6, Enote + Snote);
+    beep(buzzerPin, e6, Enote);
+    beep(buzzerPin, f6, Qnote + Enote);
+    beep(buzzerPin, b6, Enote);
+    beep(buzzerPin, c7s, Enote);
+    beep(buzzerPin, d7, Enote + Snote);
+    beep(buzzerPin, g6, Enote + Snote);
+    beep(buzzerPin, f6s, Enote);
+    beep(buzzerPin, f6, Snote + Enote);
+    beep(buzzerPin, d7, Enote + Snote);
+    beep(buzzerPin, a6s, Enote);
+    beep(buzzerPin, b6, Qnote);
+  }
 }
-
- 
